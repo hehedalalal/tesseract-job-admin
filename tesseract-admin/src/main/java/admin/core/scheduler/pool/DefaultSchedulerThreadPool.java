08259ab86d4160ca09f7ebe813f98b2f12dc1544
nickle <993831927@qq.com>
2019-07-07 09:34:19 +0800

    增加控速线程池

---
+++ tesseract-admin/src/main/java/admin/core/scheduler/pool/DefaultSchedulerThreadPool.java
@@ -0,0 +1,123 @@
+package admin.core.scheduler.pool;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 〈默认执行器线程池〉
+ *
+ * @author nickel
+ * @create 2019/6/23
+ * @since 1.0.0
+ */
+@Slf4j
+public class DefaultSchedulerThreadPool implements ISchedulerThreadPool {
+    private final int threadNum;
+    private final AtomicInteger atomicInteger = new AtomicInteger(0);
+    private final Object lock = new Object();
+
+    public DefaultSchedulerThreadPool(int threadNum) {
+        this.threadNum = threadNum;
+    }
+
+    private List<WorkerThread> availableWorkerList = Collections.synchronizedList(Lists.newArrayList());
+    private List<WorkerThread> busyWorkerList = Collections.synchronizedList(Lists.newArrayList());
+
+    @Override
+    public int blockGetAvailableThreadNum() {
+        int count;
+        synchronized (lock) {
+            while (busyWorkerList.size() == threadNum) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                }
+            }
+            count = threadNum - busyWorkerList.size();
+        }
+        return count;
+    }
+
+    @Override
+    public void runJob(Runnable runnable) {
+        synchronized (lock) {
+            //确保没有超出可用线程
+            while (busyWorkerList.size() == threadNum) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                }
+            }
+            WorkerThread worker;
+            if (availableWorkerList.size() > 0) {
+                worker = availableWorkerList.remove(0);
+            } else {
+                worker = new WorkerThread(availableWorkerList, busyWorkerList);
+                worker.start();
+                busyWorkerList.add(worker);
+            }
+            worker.setRunnable(runnable);
+        }
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    private class WorkerThread extends Thread {
+        private volatile Runnable runnable;
+        private volatile boolean stop = false;
+        private List<WorkerThread> availableWorkerList;
+        private List<WorkerThread> busyWorkerList;
+
+        public WorkerThread(List<WorkerThread> availableWorkerList, List<WorkerThread> busyWorkerList) {
+            this.availableWorkerList = availableWorkerList;
+            this.busyWorkerList = busyWorkerList;
+        }
+
+        public void setRunnable(Runnable runnable) {
+            this.runnable = runnable;
+        }
+
+        public void stopThread() {
+            this.stop = true;
+        }
+
+        @Override
+        public void run() {
+            while (!stop) {
+                if (runnable == null) {
+                    //响应是否关闭
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                    }
+                    continue;
+                }
+                try {
+                    runnable.run();
+                } catch (Exception e) {
+                } finally {
+                    runnable = null;
+                    //移动自己到空闲线程队列
+                    busyWorkerList.remove(this);
+                    availableWorkerList.add(this);
+                    //通知阻塞的线程
+                    synchronized (lock) {
+                        lock.notify();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tesseract-admin/src/main/java/admin/core/scheduler/pool/ISchedulerThreadPool.java
@@ -0,0 +1,18 @@
+package admin.core.scheduler.pool;
+
+/**
+ * 〈执行线程线程池〉
+ *
+ * @author nickel
+ * @create 2019/6/23
+ * @since 1.0.0
+ */
+public interface ISchedulerThreadPool {
+    int blockGetAvailableThreadNum();
+
+    void runJob(Runnable runnable);
+
+    void shutdown();
+
+    void init();
+}
